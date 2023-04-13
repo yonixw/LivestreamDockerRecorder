74fdacbcf8c7ca4605b18b8c0a701f9727cc8125
Yoni Water Man <yonixw@gmail.com>
2023-04-13 23:40:29 +0000

    local gitlab pipeline code

---
+++ .gitlab-ci-local-setup.sh
@@ -0,0 +1,10 @@
+# From: https://github.com/firecow/gitlab-ci-local
+
+sudo apt install -y git rsync
+yarn global add gitlab-ci-local
+gitlab-ci-local --completion >> ~/.bashrc 
+
+# For fast Builds:
+echo "alias gcl='gitlab-ci-local'" >> ~/.bashrc
+
+
