bc9ceaf7b9fa85c38df744cb8e18303ee71ccd53
Yoni Water Man <yonixw@gmail.com>
2023-05-05 02:51:25 +0000

    lint, and run custom file

--- jenkins.docker.sh
+++ jenkins.docker.sh
@@ -6,15 +6,22 @@
 # Docker entrypoint script:
 # https://github.com/jenkinsci/jenkinsfile-runner/blob/main/packaging/docker/unix/jenkinsfile-runner-launcher
 
-DOCKER_IMAGE=ghcr.io/jenkinsci/jenkinsfile-runner:latest
+# How to add jenkings plugins with custom docker
+# https://github.com/jenkinsci/jenkinsfile-runner/blob/main/docs/using/EXTENDING_DOCKER.adoc
 
+# Docker wants a jenkins file in:
+# /workspace/Jenkinsfile
+
+DOCKER_IMAGE=ghcr.io/jenkinsci/jenkinsfile-runner:latest
+JAVA_OPTS=-Xms256m
 
 sanity () {
     docker run --rm \
-        -e "JAVA_OPTS=-Xms256m" \
+        -e "JAVA_OPTS=$JAVA_OPTS" \
         -v "$(pwd):/workspace" \
         bash \
             -c " \
+            printenv && \
             ls -lah /workspace && \
             cat /workspace/Jenkinsfile && \
             echo "$@" \
@@ -23,46 +30,59 @@ sanity () {
 
 bash () {
     docker run --rm \
-        -e "JAVA_OPTS=-Xms256m" \
-        -v "$(pwd)/Jenkinsfile:/workspace/Jenkinsfile" \
+        -e "JAVA_OPTS=$JAVA_OPTS" \
+        -v "$(pwd):/workspace" \
         -it \
         --entrypoint bash \
         $DOCKER_IMAGE
 }
 
-
 base () {
     echo "Params: $@"
     docker run --rm \
-        -e "JAVA_OPTS=-Xms256m" \
+        -e "JAVA_OPTS=$JAVA_OPTS" \
         -v "$(pwd):/workspace" \
         $DOCKER_IMAGE version
     docker run --rm \
-        -e "JAVA_OPTS=-Xms256m" \
+        -e "JAVA_OPTS=$JAVA_OPTS" \
         -v "$(pwd):/workspace" \
         --entrypoint "/app/bin/jenkinsfile-runner-launcher" \
         $DOCKER_IMAGE $@
 }
 
 lint () {
-    base lint
+    # [05.05.23] For some reason, a verb (lint, run) clear flags, so we set them again.
+    base lint --jenkins-war /app/jenkins \
+    --plugins /usr/share/jenkins/ref/plugins \
+    --file /workspace/Jenkinsfile 
 }
 
 run () {
-   echo "Hello World2"
+   # No verb = run
+   base
 }
 
+runfile () {
+    # [05.05.23] For some reason, a verb (lint, run) clear flags, so we set them again.
+    base run --jenkins-war /app/jenkins \
+    --plugins /usr/share/jenkins/ref/plugins \
+    --runWorkspace /build \
+    $@
+}
 
-if [ "$1" = "sanity" ]
-then
-    sanity $@
-    exit $?
-fi
+if [ "$1" = "sanity" ]; then sanity $@; exit $? ; fi
+if [ "$1" = "bash" ]; then bash $@; exit $? ; fi
 
-if [ "$1" = "bash" ]
-then
-    bash $@
-    exit $?
-fi
+if [ "$1" = "lint" ]; then lint $@; exit $? ; fi
+if [ "$1" = "run" ]; then run $@; exit $? ; fi
 
-base $@
+# runfile expect --file relative to "pwd"
+if [ "$1" = "runfile" ]; then runfile ${@: 2}; exit $? ; fi
+
+
+echo "How to run:"
+echo -e "\tbash jenkins.docker.sh <verb> <params>"
+echo ""
+echo "Verbs:"
+echo -e "\tsanity, bash, lint, run\n\t\tNo params"
+echo -e "\trunfile\n\t\t--file /workspace/<relative to pwd>"
