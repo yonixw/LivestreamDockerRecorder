9a9ceff5b83394e57ae36b4d169c482d37d81e91
Yehonatan Water Man <yonixw@gmail.com>
2024-07-19 19:24:00 +0300

    Update delete-all.sh

--- jobs/3delete-all-vms/delete-all.sh
+++ jobs/3delete-all-vms/delete-all.sh
@@ -6,7 +6,7 @@ servers=$(./hcloud server list -o noheader -o columns=name)
 # Loop through the servers and delete them
 for server in $servers; do
   echo "Deleting server: $server"
-  ./hcloud server delete --confirm $server
+  ./hcloud server delete $server
 done
 
 echo "All servers deleted."
@@ -17,7 +17,7 @@ volumes=$(./hcloud volume list -o noheader -o columns=name)
 # Loop through the volumes and delete them
 for volume in $volumes; do
   echo "Deleting volume: $volume"
-  ./hcloud volume delete --confirm $volume
+  ./hcloud volume delete $volume
 done
 
 echo "All volumes deleted."
