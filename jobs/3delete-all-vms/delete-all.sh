15dc50e18640ef2cb141da33c04acb55e8dd4b5b
Yehonatan Water Man <yonixw@gmail.com>
2024-07-19 19:19:26 +0300

    Update delete-all.sh

--- jobs/3delete-all-vms/delete-all.sh
+++ jobs/3delete-all-vms/delete-all.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # Get a list of all servers
-servers=$(hcloud server list -o noheader -o columns=name)
+servers=$(./hcloud server list -o noheader -o columns=name)
 
 # Loop through the servers and delete them
 for server in $servers; do
   echo "Deleting server: $server"
-  hcloud server delete --confirm $server
+  ./hcloud server delete --confirm $server
 done
 
 echo "All servers deleted."
 
 # Get a list of all volumes
-volumes=$(hcloud volume list -o noheader -o columns=name)
+volumes=$(./hcloud volume list -o noheader -o columns=name)
 
 # Loop through the volumes and delete them
 for volume in $volumes; do
   echo "Deleting volume: $volume"
-  hcloud volume delete --confirm $volume
+  ./hcloud volume delete --confirm $volume
 done
 
 echo "All volumes deleted."
 
-hcloud ssh-key list |  awk '{print $1, $2}' | grep '_deploy$' | awk '{print $1}' | xargs -n1 hcloud ssh-key delete 
+./hcloud ssh-key list |  awk '{print $1, $2}' | grep '_deploy$' | awk '{print $1}' | xargs -n1 ./hcloud ssh-key delete 
 
 echo "All SSH keys deleted."
